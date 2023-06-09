0e4640707cc4c16ce0876179f90b8790fdd4888d
AYUB THAPA <68695090+AYUB-CMD@users.noreply.github.com>
2023-06-09 15:56:00 +0530

    routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:catalogue_app/screens/home_page.dart';
 import 'package:catalogue_app/screens/login_page.dart';
+import 'package:catalogue_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,16 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: const Homepage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: ThemeData(primarySwatch: Colors.purple,fontFamily: GoogleFonts.lato().fontFamily,),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: '/',
       routes: {
         "/": (context) => const LoginPage(),
-        "/home": (context) => const Homepage(),
-        '/login': (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const Homepage(),
+        MyRoutes.loginRoute: (context) => const LoginPage(),
       },
     );
   }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:catalogue_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -12,44 +13,48 @@ class LoginPage extends StatelessWidget {
           const SizedBox(height: 20),
           Image.asset(
             'assets/images/login_pages.png',
-            height: 350,
+            height: 450,
             fit: BoxFit.cover,
           ),
           const SizedBox(height: 20),
           const Text(
             'Welcome',
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 30,
               fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter a username",
-                    labelText: "Username",
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter a username",
+                      labelText: "Username",
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  obscuringCharacter: '*',
-                  decoration: const InputDecoration(
-                    hintText: "Enter a password",
-                    labelText: "Password",
+                  TextFormField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: const InputDecoration(
+                      hintText: "Enter a password",
+                      labelText: "Password",
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    print('sheesh');
-                  },
-                  child: const Text('Login'),
-                ),
-              ],
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    style:
+                        TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                    child: const Text('Login'),
+                  ),
+                ],
+              ),
             ),
           )
         ],
